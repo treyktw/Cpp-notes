e76d420f139f057c4d1f261b21ef6d30a9ddfc16
treyktw <101672317+treyktw@users.noreply.github.com>
2022-09-20 10:26:51 -0400

    Add files via upload

---
+++ useful_mathF.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+
+    double x = 3;
+    double y = 4;
+    double z;
+
+    //z = std::max(x, y);
+    //z = std::min(x, y);
+    //? pow = to the power of
+    //z = pow(2, 3);
+    //? square root
+    //z = sqrt(9);
+    //? absolute value 
+    //! puting a negative interger will result in the positive interger
+    //z = abs(3)
+    //? rounding function
+    //z = round(x);
+    //? always round up
+    //z = ceil(x);
+    //? always round down
+    //z = floor(x);
+
+    //Todo for more math functions refer to this website
+    //! https://cplusplus.com/reference/cmath/
+
+    std::cout << z;
+
+    return 0;
+}
